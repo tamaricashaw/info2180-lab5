ee6a4474391853573267db82cee300b857090e42
Tamarica Shaw <tamaricashaw13@gmail.com>
2023-11-20 18:23:29 -0500

    Fixed display for additonal table information

--- world.php
+++ world.php
@@ -13,9 +13,38 @@ $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
 
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
+
+
 ?>
-<ul>
+
+<table border="2">
+  <thead>
+    <tr>
+      <th>Name</th>
+      <th>Continent</th>
+      <th>Independence </th>
+      <th>Head of State</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php foreach ($results as $row): ?>
+      <tr>
+        <td><?= htmlspecialchars($row['name']) ?></td>
+        <td><?= htmlspecialchars($row['continent']) ?></td>
+        <td><?= htmlspecialchars($row['independence_year']) ?></td>
+        <td><?= htmlspecialchars($row['head_of_state']) ?></td>
+    
+      </tr>
+    <?php endforeach; ?>
+  </tbody>
+</table>
+
+
+
+
+
+<!-- <ul>
 <?php foreach ($results as $row): ?>
   <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
 <?php endforeach; ?>
-</ul>
+</ul> -->
